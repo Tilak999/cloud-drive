38b051e5707372cafc85a99b15e159f1df835fef
Yash Hemnani <yashh.1402+github@gmail.com>
2022-04-07 23:30:05 +0530

    Name fixed

--- src/components/FileIcons.tsx
+++ src/components/FileIcons.tsx
@@ -5,12 +5,12 @@ import { GoFile, GoFileDirectory } from "react-icons/go";
 export default function FileIcon({ filename, mimeType }) {
     const chunks = filename.split(".");
     const ext = chunks[chunks.length - 1].toLowerCase();
-    const videoFiles = ["mkv", 'mp4', 'avi', 'mov', 'flv']
+    const videoFileExts = ["mkv", 'mp4', 'avi', 'mov', 'flv']
     if (mimeType.endsWith("folder"))
         return <Icon as={GoFileDirectory} boxSize="6" verticalAlign={"bottom"} />;
     else {
         let icon = GoFile;
-        if (videoFiles.indexOf(ext.toLowerCase()) > -1) icon = FaFileVideo;
+        if (videoFileExts.indexOf(ext) > -1) icon = FaFileVideo;
         return <Icon as={icon} boxSize="6" verticalAlign={"bottom"} />;
     }
 }

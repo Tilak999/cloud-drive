73028b86eff4349fdddbbde76597f510728daa53
Yash Hemnani <yashh.1402+github@gmail.com>
2022-04-07 23:28:28 +0530

    Use video icons for video files

--- src/components/FileIcons.tsx
+++ src/components/FileIcons.tsx
@@ -5,11 +5,12 @@ import { GoFile, GoFileDirectory } from "react-icons/go";
 export default function FileIcon({ filename, mimeType }) {
     const chunks = filename.split(".");
     const ext = chunks[chunks.length - 1].toLowerCase();
+    const videoFiles = ["mkv", 'mp4', 'avi', 'mov', 'flv']
     if (mimeType.endsWith("folder"))
         return <Icon as={GoFileDirectory} boxSize="6" verticalAlign={"bottom"} />;
     else {
         let icon = GoFile;
-        if (ext === "mkv") icon = FaFileVideo;
+        if (videoFiles.indexOf(ext.toLowerCase()) > -1) icon = FaFileVideo;
         return <Icon as={icon} boxSize="6" verticalAlign={"bottom"} />;
     }
 }

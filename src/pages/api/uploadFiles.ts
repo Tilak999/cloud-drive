0ea2764a0e08f7ae654b8384bea6b1e5b2cc2ecf
Yash Hemnani <yash.1402+github@gmail.com>
2023-07-06 16:24:28 +0000

    Fixed retry logic

--- src/pages/api/uploadFiles.ts
+++ src/pages/api/uploadFiles.ts
@@ -6,34 +6,39 @@ import fs from 'fs';
 import path from 'path';
 
 const handler = async (req, res) => {
-	const form = formidable({
-		uploadDir: './public/uploads',
-		maxFileSize: 15 * 1024 * 1024 * 1024,
-		keepExtensions: true
-	});
-	const gfs = await getGFS(getToken(req, res));
-	try {
-		await form.parse(req, async (err, fields, files) => {
-			const file = files.files[0]
-			const { directoryId, relativePath } = fields;
-			const destFolderId = await getOrCreateDirectories(gfs, directoryId[0], relativePath[0]);
-			const filepath = file.filepath
-			console.log(`Uploading.. ${file.originalFilename}`);
-			await gfs.uploadFile(fs.createReadStream(file.filepath), {
-				name: file.originalFilename,
-				size: file.size,
-				progress: (e) => { },
-				parentId: destFolderId
-			});
-			fs.rm(filepath, () => console.log('File removed'));
-			console.log(`File uploaded to gDrive: ${file.originalFilename}`);
-			res.status(200).json({});
-		});
 
-	} catch (error) {
-		console.error(error);
-		res.status(500);
-	}
+	return new Promise<void>(async (resolve, reject) => {
+		const form = formidable({
+			uploadDir: './public/uploads',
+			maxFileSize: 15 * 1024 * 1024 * 1024,
+			keepExtensions: true
+		});
+		const gfs = await getGFS(getToken(req, res));
+		try {
+			await form.parse(req, async (err, fields, files) => {
+				const file = files.files[0]
+				const { directoryId, relativePath } = fields;
+				const destFolderId = await getOrCreateDirectories(gfs, directoryId[0], relativePath[0]);
+				const filepath = file.filepath
+				console.log(`Uploading.. ${file.originalFilename}`);
+				await gfs.uploadFile(fs.createReadStream(file.filepath), {
+					name: file.originalFilename,
+					size: file.size,
+					progress: (e) => { },
+					parentId: destFolderId
+				});
+				fs.rm(filepath, () => console.log('File removed'));
+				console.log(`File uploaded to gDrive: ${file.originalFilename}`);
+				res.status(200).json({});
+			});
+
+		} catch (error) {
+			console.error(error);
+			res.status(500);
+			reject()
+		}
+	})
+
 };
 
 const getOrCreateDirectories = async (gfs: GdriveFS, directoryId: string, relativePath: string) => {
